d142e37d5c8c841af4957a782d7b3145fb7aec3d
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-10-31 22:51:16 +0900

    不要なファイルを生成しない

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module Mamorukun
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
