09f25a74b37d0ee6dba14c429ada9e0075a91b6b
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-11-04 22:15:11 +0900

    deploy.rbの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
-# Gemfile.lockを見てcapistranoのバージョンを入れる
-lock '3.14.1'
-
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'mamorukun'
-set :repo_url,  'git@github.com:yuri2509/mamorukun
+set :repo_url,  'git@github.com:yuri2509/mamorukun'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
