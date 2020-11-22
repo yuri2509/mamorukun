fa34a8adf6c1f62ea400cab358daf18c4d27b54f
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-11-22 23:36:06 +0900

    deviseのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
   root "audios#index"
   resources :delivery_traders
   resources :strangers
