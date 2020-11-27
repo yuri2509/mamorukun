80ea1f1aafe80de5ca0411e43f49ba5b11beda77
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-11-28 00:22:14 +0900

    エラーメッセージの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+  
 
-  validates :nickname, :email, :encrypted_password,
+
+  validates :nickname, :email,
             presence: true, 
             uniqueness: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :password, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
+  validates :password, :encrypted_password,  presence: true
+
 end
