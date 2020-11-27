1c6e41fb2216183b6bd41232907216faa767e09c
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-11-27 22:15:00 +0900

    ハッシュロケットから変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   } 
+
   root "audios#index"
   resources :delivery_traders
   resources :strangers
