271055723bd22c3886e38c7919c0d574c7db9860
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-12-08 13:22:11 +0900

    新規登録ができた際のフラッシュメッセージの表示

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,7 +12,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to("/")
+      redirect_to root_path, notice: 'ユーザーを登録しました'
     else
       render :new
     end
