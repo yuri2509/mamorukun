3cf91cb5cf4853e615b8dea16ceb7ba22fc0f15a
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-12-13 23:51:40 +0900

    ユーザー情報編集の保存

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,11 +19,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def edit
-  
   end
 
   def update
-
+    if current_user.update(user_params)
+      redirect_to root_path, notice: 'ユーザー情報を編集しました。'
+    else
+      render :edit
+    end
   end
 
   # def show
