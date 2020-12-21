176f3fb02c85e25b1ebbf83728a6d3b431715d0a
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-12-21 21:59:51 +0900

    factory_bot記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :confirmable
+         :recoverable, :rememberable
   
   validates :nickname, :email,
             uniqueness: true
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+
+    it "nikcnameがない場合は登録できないこと" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("を入力してください")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+  end
+end
