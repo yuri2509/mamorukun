dabdf3d37d89bb03a8ef323a36332e8122a9bbc3
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-12-22 23:01:16 +0900

    user_controllerのeditのテスト

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,4 +44,21 @@ RSpec.describe "UserAuthentications", type: :request do
       end
     end
   end
+
+  describe 'GET #edit' do
+    subject { get edit_user_registration_path }
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      it 'リクエストが成功すること' do
+        is_expected.to eq 200
+      end
+    end
+    context 'ゲストの場合' do
+      it 'リダイレクトされること' do
+        is_expected.to redirect_to new_user_session_path
+      end
+    end
+  end
 end
