5b9199c4d2ed457d792a9831dc7696966f10da79
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-01-04 01:53:15 +0900

    投稿の保存できたときとできなかったときの条件分岐

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,5 +7,19 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @messege = Message.new(message_params)
+    if @messege.save
+      redirect_to messages_path, notice: '投稿しました。'
+    else
+      redirect_to messages_path, notice: 'メッセージを入力してください。'
+    end
   end
 end
+
+
+private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+  
