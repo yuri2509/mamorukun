59fb9f3084a7af159a2622f6396792d97685b268
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-01-08 00:58:20 +0900

    投稿時間の表示方法変更

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Mamorukun
     
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
