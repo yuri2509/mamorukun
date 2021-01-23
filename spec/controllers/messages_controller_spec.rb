d14dfc25c537039959a4246d8adafc8b3ecd6a8a
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-01-23 23:03:17 +0900

    indexのテストの変更

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "MessagesController", type: :request do
   let(:user) { create(:user) }
   let(:message) { create(:message) }
-
-  context "ログインしていないこと" do
-    describe "#index" do
+  
+  describe "GET#index" do
+    context "ログインしていないこと" do
       it "正常なレスポンスであること" do
         get messages_url
         expect(response).to be_successful
@@ -15,35 +15,39 @@ RSpec.describe "MessagesController", type: :request do
         expect(response).to have_http_status "200"
       end
     end
-  end
 
-  context "ログイン済みであること" do
-    before do
-      sign_in user
-    end
-    context "投稿一覧ページが正しく表示されること" do
+    context "ログイン済みであること" do
       before do
-        get messages_path
+        sign_in user
       end
+      context "投稿一覧ページが正しく表示されること" do
+        before do
+          get messages_path
+        end
 
-      it "正常なレスポンスであること" do
-        get messages_url
-        expect(response).to be_successful
-      end
-      it "200レスポンスが返ってきていること" do
-        get messages_url
-        expect(response).to have_http_status "200"
-      end
+        it "正常なレスポンスであること" do
+          get messages_url
+          expect(response).to be_successful
+        end
+        it "200レスポンスが返ってきていること" do
+          get messages_url
+          expect(response).to have_http_status "200"
+        end
 
-      it "@messageに正しい値が入っていること" do
-        messages = create_list(:message, 3) 
-        # expect(assigns(:messages)).to eq messages
-        expect(assigns(:messages).attributes).to eq(blank_message.attributes)
-      end
+        it '@messageに期待した値が入っていること' do
+          expect(assigns(:message)).to be_a_new(Message)
+        end
 
-      it 'メッセージが正しく表示されていること' do
-        expect(response.message).to include(message)
+        it "@messagesに正しい値が入っていること" do
+          messages = create_list(:message, 3) 
+          # expect(assigns(:messages)).to eq messages
+          # expect(assigns(:messages)).to match(messages.sort{ |a, b| b.created_at <=> a.created_at } )
+          expect(assigns(:messages).attributes).to eq(blank_message.attributes)
+        end
 
+        it 'メッセージが正しく表示されていること' do
+          expect(response.message).to include(message)
+        end
       end
     end
   end
