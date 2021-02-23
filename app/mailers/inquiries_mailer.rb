7f5286a280f660e44b6771f899988993882c0028
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-02-23 22:35:18 +0900

    メール設定

--- app/mailers/inquiries_mailer.rb
+++ app/mailers/inquiries_mailer.rb
@@ -1,2 +1,9 @@
 class InquiriesMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+  
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'お問い合わせ')
+  end
 end
